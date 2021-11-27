6335b0e4d750713a2e7f718e312fc3fe8f315fd2
RabiusRaju <rabiussani8121@gmail.com>
2021-11-27 12:58:28 +0600

    Shopping Dao create wip

--- app/src/main/java/com/example/todomvvm/db/AppDatabase.java
+++ app/src/main/java/com/example/todomvvm/db/AppDatabase.java
@@ -1,4 +1,24 @@
 package com.example.todomvvm.db;
 
-public class AppDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Category.class,Items.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public static AppDatabase INSTANCE;
+
+    public static AppDatabase getDBinstance(Context context){
+        if(INSTANCE == null){
+            /*
+            * Database Class Build this
+            * */
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class,"AppDB")
+                        .allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
 }
